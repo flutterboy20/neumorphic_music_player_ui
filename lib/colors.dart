5b9de5e3a4dc3d93bf646e68524bfe88d6072020
Himanshu <ngukumar123@gmail.com>
2022-07-30 14:03:01 +0530

    Solve the Bottom error using singlechildscrollview in audio player screen

--- lib/colors.dart
+++ lib/colors.dart
@@ -6,12 +6,8 @@ const Color backgroundColor = Color(0xFFDFE9FF);
 const Color primarybuttonColor = Color(0xFFE3ECFD);
 const Color secondaryButtonColor = Color(0xFFDCE5F6);
 
-const Color iconColor = Color(0xFF8E98AE);
+const Color iconColor = Color(0xFF383CC1);
 
 const Color primaryActiveButtonColor = Colors.deepOrange;
 
 const Color secondaryActiveButtonColor = Colors.deepOrangeAccent;
-
-
-
-
--- lib/screen/audio_player_screen.dart
+++ lib/screen/audio_player_screen.dart
@@ -18,8 +18,6 @@ class AudioPlayerScreen extends StatefulWidget {
 
 class _AudioPlayerScreenState extends State<AudioPlayerScreen>
     with TickerProviderStateMixin {
-
-
   late final AnimationController _controller = AnimationController(
     duration: const Duration(seconds: 5),
     vsync: this,
@@ -43,158 +41,159 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen>
     return SafeArea(
       child: Scaffold(
         backgroundColor: backgroundColor,
-        body: Column(
-          // Wrap this column with singlechildscrollview if you face overflow issue
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(22),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CustomButton(
-                        primaryColor: primarybuttonColor,
-                        secondaryColor: secondaryButtonColor,
-                        child: GestureDetector(
-                          onTap: () => Navigator.of(context).pop(),
-                          child: const Icon(
-                            Icons.arrow_back,
+        body: SingleChildScrollView(
+          child: Column(
+            // Wrap this column with singlechildscrollview if you face overflow issue
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(22),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomButton(
+                          primaryColor: primarybuttonColor,
+                          secondaryColor: secondaryButtonColor,
+                          child: GestureDetector(
+                            onTap: () => Navigator.of(context).pop(),
+                            child: const Icon(
+                              Icons.arrow_back,
+                              color: iconColor,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          'Now Playing',
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: iconColor),
+                        ),
+                        const CustomButton(
+                          primaryColor: primarybuttonColor,
+                          secondaryColor: secondaryButtonColor,
+                          child: Icon(
+                            Icons.menu_rounded,
                             color: iconColor,
                           ),
                         ),
-                      ),
-                      const Text(
-                        'Now Playing',
-                        style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            color: iconColor),
-                      ),
-                      const CustomButton(
-                        primaryColor: primarybuttonColor,
-                        secondaryColor: secondaryButtonColor,
-                        child: Icon(
-                          Icons.menu_rounded,
-                          color: iconColor,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 50),
-                RotationTransition(
-                  turns: _animation,
-                  child: CustomNeumorphicImage(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: Image.asset(
-                        widget.songDetails.imageUrl,
-                        height: 200,
-                        width: 200,
-                        fit: BoxFit.cover,
+                  const SizedBox(height: 50),
+                  RotationTransition(
+                    turns: _animation,
+                    child: CustomNeumorphicImage(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset(
+                          widget.songDetails.imageUrl,
+                          height: 200,
+                          width: 200,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 30),
-                SongName(widget: widget),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          const Text(
-                            "2:12",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: iconColor,
+                  const SizedBox(height: 30),
+                  SongName(widget: widget),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            const Text(
+                              "2:12",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: iconColor,
+                              ),
                             ),
-                          ),
-                          Text(
-                            widget.songDetails.songDuration,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: iconColor,
+                            Text(
+                              widget.songDetails.songDuration,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: iconColor,
+                              ),
                             ),
+                          ],
+                        ),
+                        const CustomNeumorphicSlider(),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const CustomButton(
+                          primaryColor: primarybuttonColor,
+                          secondaryColor: secondaryButtonColor,
+                          padding: EdgeInsets.all(24),
+                          child: Icon(
+                            Icons.fast_rewind_rounded,
+                            color: iconColor,
                           ),
-                        ],
-                      ),
-                      const CustomNeumorphicSlider(),
-                    ],
+                        ),
+                        CustomButton(
+                          primaryColor: primaryActiveButtonColor,
+                          secondaryColor: secondaryActiveButtonColor,
+                          padding: const EdgeInsets.all(24),
+                          child: GestureDetector(
+                            onTap: () {
+                              if (_controller.isAnimating) {
+                                _controller.stop(); // Stop the animation
+                                setState(() {
+                                  isStopped = !isStopped;
+                                });
+                              } else {
+                                _controller.repeat(); // Start the animation
+                                setState(() {
+                                  isStopped = !isStopped;
+                                });
+                              }
+                            },
+                            child: isStopped
+                                ? const Icon(
+                                    Icons.play_arrow_rounded,
+                                    color: Colors.black87,
+                                  )
+                                : const Icon(
+                                    Icons.pause,
+                                    color: Colors.black87,
+                                  ),
+                          ),
+                        ),
+                        const CustomButton(
+                          primaryColor: primarybuttonColor,
+                          secondaryColor: secondaryButtonColor,
+                          padding: EdgeInsets.all(24),
+                          child: Icon(
+                            Icons.fast_forward,
+                            color: iconColor,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const CustomButton(
-                        primaryColor: primarybuttonColor,
-                        secondaryColor: secondaryButtonColor,
-                        padding: EdgeInsets.all(24),
-                        child: Icon(
-                          Icons.fast_rewind_rounded,
-                          color: iconColor,
-                        ),
-                      ),
-                      CustomButton(
-                        primaryColor: primaryActiveButtonColor,
-                        secondaryColor: secondaryActiveButtonColor,
-                        padding: const EdgeInsets.all(24),
-                        child: GestureDetector(
-                          onTap: () {
-                            if (_controller.isAnimating) {
-                              _controller.stop(); // Stop the animation
-                              setState(() {
-                                isStopped = !isStopped;
-                              });
-                            } else {
-                              _controller.repeat(); // Start the animation
-                              setState(() {
-                                isStopped = !isStopped;
-                              });
-                            }
-                          },
-                          child: isStopped
-                              ? const Icon(
-                                  Icons.play_arrow_rounded,
-                                  color: Colors.black87,
-                                )
-                              : const Icon(
-                                  Icons.pause,
-                                  color: Colors.black87,
-                                ),
-                        ),
-                      ),
-                      const CustomButton(
-                        primaryColor: primarybuttonColor,
-                        secondaryColor: secondaryButtonColor,
-                        padding: EdgeInsets.all(24),
-                        child: Icon(
-                          Icons.fast_forward,
-                          color: iconColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
